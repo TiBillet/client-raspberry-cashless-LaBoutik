1c0f24805eec9f898e25b8bfb90fc301abdbef90
filaos974 <filaos974@hotmail.com>
2024-10-08 11:43:19 +0400

    modif couleur text pour noNetwork

--- nfcServer/www/assets/js/modules/templateRender.js
+++ nfcServer/www/assets/js/modules/templateRender.js
@@ -197,8 +197,8 @@ function showNoNetwork(state) {
                <path d="M12 3q2 .5 2 6q0 .506-.017.968m-.55 3.473Q12.934 14.766 12 15m0-12q-1.405.351-1.822 3.167m-.16 3.838Q10.192 14.549 12 15M6 9h3m4 0h5M3 20h7m4 0h7m-11 0a2 2 0 1 0 4 0a2 2 0 0 0-4 0m2-5v3M3 3l18 18"/>
            </g>
        </svg>
-       <p>no connection.</p>
-       <p>Check your network.</p>
+       <p class="c-white">no connection.</p>
+       <p class="c-white">Check your network.</p>
    </div>`
   } else {
     return ''
